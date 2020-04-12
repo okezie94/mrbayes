b213988c76886b7a27b228b4e6602260d8381180
Tom Palmer <remlapmot@hotmail.com>
2020-04-12 14:56:16 +0100

    Adding descriptions

--- R/mr_radialegger_rjags.R
+++ R/mr_radialegger_rjags.R
@@ -1,5 +1,7 @@
 #' Bayesian radial MR-Egger model with a choice of prior distributions fitted using JAGS.
 #'
+#' Bayesian radial MR-Egger model with a choice of prior distributions fitted using JAGS.
+#'
 #' @param object A data object of class mr_format
 #' @param prior A character string for selecting the prior distributions; "default" selects a non-informative set of priors; "weak" selects weakly informative priors; "pseudo" selects a pseudo-horseshoe prior on the causal effect, "joint" selects a joint prior on the intercept and slope.
 #' @param betaprior A character string in JAGS syntax to allow a user defined prior for the causal effect.
--- R/mrbayes-package.r
+++ R/mrbayes-package.r
@@ -1,5 +1,7 @@
 #' mrbayes: Bayesian implementation of the IVW and MR-Egger models for two-sample Mendelian randomization analyses
 #'
+#' Bayesian implementation of the IVW and MR-Egger models for two-sample Mendelian randomization analyses.
+#'
 #' @name mrbayes-package
 #' @docType package
 NULL
