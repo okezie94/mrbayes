169511c82cc8fc279bf4b79bdce43a6a4aa19e62
Tom Palmer <remlapmot@hotmail.com>
2020-04-12 15:33:47 +0100

    Update mrbayes-package.r

--- R/mrbayes-package.r
+++ R/mrbayes-package.r
@@ -4,4 +4,12 @@
 #'
 #' @name mrbayes-package
 #' @docType package
+#' @aliases mrbayes
+#' @useDynLib mrbayes, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2019). RStan: the R interface to Stan. R package version 2.19.2. https://mc-stan.org
 NULL
