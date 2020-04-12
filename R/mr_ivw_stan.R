7084830f947fbb35f5f854e86b7c3b21b2428510
Tom Palmer <remlapmot@hotmail.com>
2020-04-12 22:28:03 +0100

    @export mr_ivw_stan

--- R/mr_ivw_stan.R
+++ R/mr_ivw_stan.R
@@ -1,12 +1,13 @@
 #' Bayesian inverse variance weighted model with a choice of prior distributions fitted using RSTAN.
 #'
+#' Bayesian inverse variance weighted model with a choice of prior distributions fitted using RSTAN.
+#'
 #' @param data A data of class mr_format
 #' @param prior An integer for selecting the prior distributions; "1" selects a non-informative set of priors; "2" selects weakly informative priors; "3" selects a pseudo-horseshoe prior on the causal effect.
 #' @param n.chains Numeric indicating the number of chains used in the HMC estimation in rstan, the default is 1 chain.
 #' @param n.burn Numeric indicating the burn-in period of the Bayesian HMC estimation. The default is 200 samples.
 #' @param n.iter Numeric indicating the number of iterations in the Bayesian MCMC estimation. The default is 1000 iterations.
 #' @param seed Numeric indicating the random number seed. The default is 234.
-#'
 #'
 #' @references Burgess, S., Butterworth, A., Thompson S.G. Mendelian randomization analysis with multiple genetic variants using summarized data. Genetic Epidemiology, 2013, 37, 7, 658-665 <https://dx.doi.org/10.1002/gepi.21758>.
 #'
@@ -19,6 +20,7 @@
 #'           yse = bmi_insulin[,"se.outcome"])
 #' ivw_trial<- mr_ivw_stan(dat)
 #' print(ivw_trial)
+#' @export
 
 
 mr_ivw_stan <- function(data,
