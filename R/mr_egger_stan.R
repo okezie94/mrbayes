ba27d27592aa8b5a720de813ec4ac8b9e3d8e536
Tom Palmer <remlapmot@hotmail.com>
2020-04-15 14:31:49 +0100

    @export mr_egger_stan

--- R/mr_egger_stan.R
+++ R/mr_egger_stan.R
@@ -1,5 +1,7 @@
 #' Bayesian inverse variance weighted model with a choice of prior distributions fitted using RSTAN.
 #'
+#' Bayesian inverse variance weighted model with a choice of prior distributions fitted using RSTAN.
+#'
 #' @param data A data of class mr_format
 #' @param prior An integer for selecting the prior distributions; "1" selects a non-informative set of priors; "2" selects weakly informative priors; "3" selects a pseudo-horseshoe prior on the causal effect; "4" selects joint prior of the intercept and causal effect estimate.
 #' @param n.chains Numeric indicating the number of chains used in the HMC estimation in rstan, the default is 1 chain.
@@ -9,6 +11,8 @@
 #'
 #'
 #' @references Burgess, S., Butterworth, A., Thompson S.G. Mendelian randomization analysis with multiple genetic variants using summarized data. Genetic Epidemiology, 2013, 37, 7, 658-665 <https://dx.doi.org/10.1002/gepi.21758>.
+#'
+#' @export
 #'
 #' @examples
 #' data(bmi_insulin)
@@ -28,19 +32,19 @@ mr_egger_stan <- function(data,
                         n.iter = 1000,
                         seed = 234,
                         rho = 0.5) {
-  
+
   # check class of object
   if (!("mr_format" %in% class(data))) {
     stop(
       'The class of the data object must be "mr_format", please resave the object with the output of e.g. object <- mr_format(object).'
     )
   }
-  
+
   if (prior == 4){
     pars <- c("eta","sigma")
   } else {pars <- c("intercept","estimate","sigma")}
-  
-  
+
+
   # converting dataset to a list
   datam <- list(
     n = nrow(data),
@@ -49,8 +53,8 @@ mr_egger_stan <- function(data,
     weights = 1/data[, 5],
     prior = prior, rho = rho
   )
-  
-  
+
+
   eggerfit <- rstan::sampling(
     object = stanmodels$mregger,
     data = datam,
@@ -63,5 +67,5 @@ mr_egger_stan <- function(data,
   )
 
   return(eggerfit)
-  
+
 }
