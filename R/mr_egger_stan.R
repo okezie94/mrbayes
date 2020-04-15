6e2c45df0ada36842bbaeff93a8839bcb1f21ad6
Tom Palmer <remlapmot@hotmail.com>
2020-04-15 14:30:44 +0100

    Create mr_egger_stan.R

---
+++ R/mr_egger_stan.R
@@ -0,0 +1,67 @@
+#' Bayesian inverse variance weighted model with a choice of prior distributions fitted using RSTAN.
+#'
+#' @param data A data of class mr_format
+#' @param prior An integer for selecting the prior distributions; "1" selects a non-informative set of priors; "2" selects weakly informative priors; "3" selects a pseudo-horseshoe prior on the causal effect; "4" selects joint prior of the intercept and causal effect estimate.
+#' @param n.chains Numeric indicating the number of chains used in the HMC estimation in rstan, the default is 1 chain.
+#' @param n.burn Numeric indicating the burn-in period of the Bayesian HMC estimation. The default is 200 samples.
+#' @param n.iter Numeric indicating the number of iterations in the Bayesian HMC estimation. The default is 1000 iterations.
+#' @param seed Numeric indicating the random number seed. The default is 234.
+#'
+#'
+#' @references Burgess, S., Butterworth, A., Thompson S.G. Mendelian randomization analysis with multiple genetic variants using summarized data. Genetic Epidemiology, 2013, 37, 7, 658-665 <https://dx.doi.org/10.1002/gepi.21758>.
+#'
+#' @examples
+#' data(bmi_insulin)
+#' dat <- mr_format(rsid = bmi_insulin[,"rsid"],
+#'           xbeta = bmi_insulin[,"beta.exposure"],
+#'           ybeta = bmi_insulin[,"beta.outcome"],
+#'           xse = bmi_insulin[,"se.exposure"],
+#'           yse = bmi_insulin[,"se.outcome"])
+#' egger_trial<- mr_egger_stan(dat)
+#' print(egger_trial)
+
+
+mr_egger_stan <- function(data,
+                        prior = 1,
+                        n.chains = 1,
+                        n.burn = 200,
+                        n.iter = 1000,
+                        seed = 234,
+                        rho = 0.5) {
+  
+  # check class of object
+  if (!("mr_format" %in% class(data))) {
+    stop(
+      'The class of the data object must be "mr_format", please resave the object with the output of e.g. object <- mr_format(object).'
+    )
+  }
+  
+  if (prior == 4){
+    pars <- c("eta","sigma")
+  } else {pars <- c("intercept","estimate","sigma")}
+  
+  
+  # converting dataset to a list
+  datam <- list(
+    n = nrow(data),
+    xbeta = data[, 2]/data[, 5],
+    ybeta = data[, 3]/data[, 5],
+    weights = 1/data[, 5],
+    prior = prior, rho = rho
+  )
+  
+  
+  eggerfit <- rstan::sampling(
+    object = stanmodels$mregger,
+    data = datam,
+    pars = pars,
+    chains = n.chains,
+    warmup = n.burn,
+    iter = n.iter,
+    seed = seed,
+    control = list(adapt_delta = 0.999, max_treedepth = 15)
+  )
+
+  return(eggerfit)
+  
+}
