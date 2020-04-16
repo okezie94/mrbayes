3f2d7f0f80cd2b8b103bdb655da9b47a24819ae7
Tom Palmer <remlapmot@hotmail.com>
2020-04-16 10:04:14 +0100

    Update test-mr_ivw_stan.R

--- tests/testthat/test-mr_ivw_stan.R
+++ tests/testthat/test-mr_ivw_stan.R
@@ -14,45 +14,30 @@ test_that("IVW using default prior method",
             expect_equal(summfit$summary["estimate","sd"], 0.05, tol = 1e-2) # 0.04 ??
             expect_equal(summfit$summary["estimate","2.5%"], 0.38, tol = 1e-2) # .44 ??
             expect_equal(summfit$summary["estimate","50%"], 0.50, tol = 1e-2)
-            expect_equal(summfit$summary["estimate","97.5%"], 0.61, tol = 1e-2) # .58
+            expect_equal(summfit$summary["estimate","97.5%"], 0.61, tol = 1e-2) # .58 ??
           })
 #
 
 test_that("IVW using weak prior method",
           {
-            ivwfit1 <- mr_ivw_stan(mrdat, prior = "weak", seed = 123)
-            expect_equal(class(ivwfit1), "ivwjags")
-            expect_equal(unname(ivwfit1$CausalEffect), 0.5, tol = 1e-2)
-            expect_equal(unname(ivwfit1$StandardError), 0.04, tol = 1e-2)
-            expect_equal(unname(ivwfit1$CredibleInterval[1]), 0.44, tol = 1e-2)
-            expect_equal(unname(ivwfit1$CredibleInterval[2]), 0.5, tol = 1e-2)
-            expect_equal(unname(ivwfit1$CredibleInterval[3]), 0.58, tol = 1e-2)
-            expect_equal(class(ivwfit1$samples), "mcmc.list")
-            expect_equal(ivwfit1$priormethod, "weak")
+            ivwfit1 <- mr_ivw_stan(do_data, prior = 2, seed = 123)
+            summfit1 <- rstan::summary(ivwfit1)
+            expect_equal(class(ivwfit1)[1], "stanfit")
+            expect_equal(summfit1$summary["estimate","mean"], 0.5, tol = 1e-2)
+            expect_equal(summfit1$summary["estimate","sd"], 0.058, tol = 1e-2) # .04
+            expect_equal(summfit1$summary["estimate","2.5%"], 0.38, tol = 1e-2) # .44
+            expect_equal(summfit1$summary["estimate","50%"], 0.5, tol = 1e-2)
+            expect_equal(summfit1$summary["estimate","97.5%"], 0.61, tol = 1e-2) # .58
           })
 
 test_that("IVW using pseudo prior method",
           {
-            ivwfit2 <- mr_ivw_stan(mrdat, prior = "pseudo", seed = 123)
-            expect_equal(class(ivwfit2), "ivwjags")
-            expect_equal(unname(ivwfit2$CausalEffect), 0.5, tol = 1e-2)
-            expect_equal(unname(ivwfit2$StandardError), 0.03, tol = 1e-2)
-            expect_equal(unname(ivwfit2$CredibleInterval[1]), 0.43, tol = 1e-2)
-            expect_equal(unname(ivwfit2$CredibleInterval[2]), 0.50, tol = 1e-2)
-            expect_equal(unname(ivwfit2$CredibleInterval[3]), 0.57, tol = 1e-2)
-            expect_equal(class(ivwfit2$samples), "mcmc.list")
-            expect_equal(ivwfit2$priormethod, "pseudo")
-          })
-
-
-test_that("IVW using beta prior method",
-          {
-            ivwfit3 <- mr_ivw_stan(mrdat, betaprior = "dnorm(0, 1E-6)", seed = 123)
-            expect_equal(class(ivwfit3), "ivwjags")
-            expect_equal(unname(ivwfit3$CausalEffect), 0.5, tol = 1e-2)
-            expect_equal(unname(ivwfit3$StandardError), 0.04, tol = 1e-2)
-            expect_equal(unname(ivwfit3$CredibleInterval[1]), 0.44, tol = 1e-2)
-            expect_equal(unname(ivwfit3$CredibleInterval[2]), 0.5, tol = 1e-2)
-            expect_equal(unname(ivwfit3$CredibleInterval[3]), 0.58, tol = 1e-2)
-            expect_equal(class(ivwfit3$samples), "mcmc.list")
+            ivwfit2 <- mr_ivw_stan(do_data, prior = 3, seed = 123)
+            summfit2 <- rstan::summary(ivwfit2)
+            expect_equal(class(ivwfit2)[1], "stanfit")
+            expect_equal(summfit2$summary["estimate","mean"], 0.5, tol = 1e-2)
+            expect_equal(summfit2$summary["estimate","sd"], 0.057, tol = 1e-2) # .03
+            expect_equal(summfit2$summary["estimate","2.5%"], 0.387, tol = 1e-2) # .43
+            expect_equal(summfit2$summary["estimate","50%"], 0.50, tol = 1e-2)
+            expect_equal(summfit2$summary["estimate","97.5%"], 0.605, tol = 1e-2) # .57
           })
