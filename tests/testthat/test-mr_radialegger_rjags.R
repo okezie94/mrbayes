583bd92f0974d09fc2de17a49371db881f69c48b
Tom Palmer <remlapmot@hotmail.com>
2020-04-16 10:48:55 +0100

    Update test-mr_radialegger_rjags.R

--- tests/testthat/test-mr_radialegger_rjags.R
+++ tests/testthat/test-mr_radialegger_rjags.R
@@ -2,15 +2,10 @@
 
 context("Tests for Radial MR-Egger function using JAGS")
 
-test_that("Check the class of the data object",
-          {
-            mrdat <- with(do_data, mr_format(rsid, ldlcbeta, chdbeta, ldlcse, chdse))
-            expect_equal(class(mrdat), c("data.frame","mr_format"))
-          })
-
 test_that("Radial-Egger using default prior method",
           {
-            radialeggerfit <- mr_radialegger_rjags(mrdat, seed = 123)
+            radialeggerfit <- mr_radialegger_rjags(do_data,
+                                                   seed = c(123, 456, 789))
             expect_equal(class(radialeggerfit), "radialeggerjags")
             expect_equal(unname(radialeggerfit$CausalEffect), 0.51, tol = 1e-2)
             expect_equal(unname(radialeggerfit$StandardError), 0.058, tol = 1e-2)
@@ -23,10 +18,11 @@ test_that("Radial-Egger using default prior method",
             expect_equal(radialeggerfit$priormethod, "default")
           })
 
-
 test_that("Radial-Egger using weak prior method",
           {
-            radialeggerfit1 <- mr_radialegger_rjags(mrdat, prior = "weak", seed = 123)
+            radialeggerfit1 <- mr_radialegger_rjags(do_data,
+                                                    prior = "weak",
+                                                    seed = c(123, 456, 789))
             expect_equal(class(radialeggerfit1), "radialeggerjags")
             expect_equal(unname(radialeggerfit1$CausalEffect), 0.51, tol = 1e-2)
             expect_equal(unname(radialeggerfit1$StandardError), 0.058, tol = 1e-2)
@@ -39,10 +35,11 @@ test_that("Radial-Egger using weak prior method",
             expect_equal(radialeggerfit1$priormethod, "weak")
           })
 
-
 test_that("Radial-Egger using pseudo prior method",
           {
-            radialeggerfit2 <- mr_radialegger_rjags(mrdat, prior = "pseudo", seed = 123)
+            radialeggerfit2 <- mr_radialegger_rjags(do_data,
+                                                    prior = "pseudo",
+                                                    seed = c(123, 456, 789))
             expect_equal(class(radialeggerfit2), "radialeggerjags")
             expect_equal(unname(radialeggerfit2$CausalEffect), 0.51, tol = 1e-2)
             expect_equal(unname(radialeggerfit2$StandardError), 0.058, tol = 1e-2)
@@ -55,33 +52,38 @@ test_that("Radial-Egger using pseudo prior method",
             expect_equal(radialeggerfit2$priormethod, "pseudo")
           })
 
-
 test_that("Radial-Egger using joint prior method",
           {
             skip_on_cran()
-            radialeggerfit2 <- mr_radialegger_rjags(mrdat, prior = "joint", seed = 123, rho = 0.5)
-            expect_equal(class(radialeggerfit2), "radialeggerjags")
-            expect_equal(unname(radialeggerfit2$CausalEffect), 0.5, tol = 1e-2)
-            expect_equal(unname(radialeggerfit2$StandardError), 0.058, tol = 1e-2)
-            expect_equal(unname(radialeggerfit2$CredibleInterval[1]), 0.392, tol = 1e-2)
-            expect_equal(unname(radialeggerfit2$CredibleInterval[2]), 0.505, tol = 1e-2)
-            expect_equal(unname(radialeggerfit2$CredibleInterval[3]), 0.613, tol = 1e-2)
-            expect_equal(unname(radialeggerfit2$AvgPleio), -0.0654, tol = 1e-2 )
-            expect_equal(unname(radialeggerfit2$sigma), 0.406, tol = 1e-2)
-            expect_equal(class(radialeggerfit2$samples), "mcmc.list")
-            expect_equal(radialeggerfit2$priormethod, "joint")
+            radialeggerfit3 <- mr_radialegger_rjags(do_data,
+                                                    prior = "joint",
+                                                    seed = c(123, 456, 789),
+                                                    rho = 0.5)
+            expect_equal(class(radialeggerfit3), "radialeggerjags")
+            expect_equal(unname(radialeggerfit3$CausalEffect), 0.5, tol = 1e-2)
+            expect_equal(unname(radialeggerfit3$StandardError), 0.058, tol = 1e-2)
+            expect_equal(unname(radialeggerfit3$CredibleInterval[1]), 0.392, tol = 1e-2)
+            expect_equal(unname(radialeggerfit3$CredibleInterval[2]), 0.505, tol = 1e-2)
+            expect_equal(unname(radialeggerfit3$CredibleInterval[3]), 0.613, tol = 1e-2)
+            expect_equal(unname(radialeggerfit3$AvgPleio), -0.0654, tol = 1e-2 )
+            expect_equal(unname(radialeggerfit3$sigma), 0.406, tol = 1e-2)
+            expect_equal(class(radialeggerfit3$samples), "mcmc.list")
+            expect_equal(radialeggerfit3$priormethod, "joint")
           })
 
 test_that("Radial-Egger using betaprior and sigmaprior method",
           {
-            radialeggerfit1 <- mr_radialegger_rjags(mrdat, betaprior = "dnorm(0, 1E-6)", sigmaprior = "dunif(.0001, 10)", seed = 123)
-            expect_equal(class(radialeggerfit1), "radialeggerjags")
-            expect_equal(unname(radialeggerfit1$CausalEffect), 0.51, tol = 1e-2)
-            expect_equal(unname(radialeggerfit1$StandardError), 0.058, tol = 1e-2)
-            expect_equal(unname(radialeggerfit1$CredibleInterval[1]),0.393 , tol = 1e-2)
-            expect_equal(unname(radialeggerfit1$CredibleInterval[2]), 0.505, tol = 1e-2)
-            expect_equal(unname(radialeggerfit1$CredibleInterval[3]), 0.619, tol = 1e-2)
-            expect_equal(unname(radialeggerfit1$AvgPleio), -0.0633, tol = 1e-2 )
-            expect_equal(unname(radialeggerfit1$sigma), 0.404, tol = 1e-2)
-            expect_equal(class(radialeggerfit1$samples), "mcmc.list")
+            radialeggerfit4 <- mr_radialegger_rjags(do_data,
+                                                    betaprior = "dnorm(0, 1E-6)",
+                                                    sigmaprior = "dunif(.0001, 10)",
+                                                    seed = c(123, 456, 789))
+            expect_equal(class(radialeggerfit4), "radialeggerjags")
+            expect_equal(unname(radialeggerfit4$CausalEffect), 0.51, tol = 1e-2)
+            expect_equal(unname(radialeggerfit4$StandardError), 0.058, tol = 1e-2)
+            expect_equal(unname(radialeggerfit4$CredibleInterval[1]),0.393 , tol = 1e-2)
+            expect_equal(unname(radialeggerfit4$CredibleInterval[2]), 0.505, tol = 1e-2)
+            expect_equal(unname(radialeggerfit4$CredibleInterval[3]), 0.619, tol = 1e-2)
+            expect_equal(unname(radialeggerfit4$AvgPleio), -0.0633, tol = 1e-2 )
+            expect_equal(unname(radialeggerfit4$sigma), 0.404, tol = 1e-2)
+            expect_equal(class(radialeggerfit4$samples), "mcmc.list")
           })
