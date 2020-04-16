343c5c79d4a2b86bb9c2c1f5f1aa586fb5ccd04c
Tom Palmer <remlapmot@hotmail.com>
2020-04-16 17:21:26 +0100

    Stan and formatting

--- R/mr_egger_stan.R
+++ R/mr_egger_stan.R
@@ -1,14 +1,19 @@
-#' Bayesian inverse variance weighted model with a choice of prior distributions fitted using RSTAN.
+#' Bayesian inverse variance weighted model with a choice of prior distributions fitted using Stan
 #'
-#' Bayesian inverse variance weighted model with a choice of prior distributions fitted using RSTAN.
+#' Bayesian inverse variance weighted model with a choice of prior distributions fitted using Stan.
 #'
-#' @param data A data of class mr_format
-#' @param prior An integer for selecting the prior distributions; "1" selects a non-informative set of priors; "2" selects weakly informative priors; "3" selects a pseudo-horseshoe prior on the causal effect; "4" selects joint prior of the intercept and causal effect estimate.
-#' @param n.chains Numeric indicating the number of chains used in the HMC estimation in rstan, the default is 1 chain.
-#' @param n.burn Numeric indicating the burn-in period of the Bayesian HMC estimation. The default is 200 samples.
-#' @param n.iter Numeric indicating the number of iterations in the Bayesian HMC estimation. The default is 1000 iterations.
-#' @param rho Numeric indicating the correlation coefficient input into the joint prior distribution. The default is 0.5.
-#' @param seed Numeric indicating the random number seed. The default is 234.
+#' @param data A data of class [`mr_format`].
+#' @param prior An integer for selecting the prior distributions;
+#'
+#' * `1` selects a non-informative set of priors;
+#' * `2` selects weakly informative priors;
+#' * `3` selects a pseudo-horseshoe prior on the causal effect;
+#' * `4` selects joint prior of the intercept and causal effect estimate.
+#' @param n.chains Numeric indicating the number of chains used in the HMC estimation in rstan, the default is `3` chains.
+#' @param n.burn Numeric indicating the burn-in period of the Bayesian HMC estimation. The default is `1000` samples.
+#' @param n.iter Numeric indicating the number of iterations in the Bayesian HMC estimation. The default is `5000` iterations.
+#' @param rho Numeric indicating the correlation coefficient input into the joint prior distribution. The default is `0.5`.
+#' @param seed Numeric indicating the random number seed. The default is `12345`.
 #'
 #' @return An object of class [`stanfit`].
 #'
@@ -22,10 +27,10 @@
 
 mr_egger_stan <- function(data,
                         prior = 1,
-                        n.chains = 1,
-                        n.burn = 200,
-                        n.iter = 1000,
-                        seed = 234,
+                        n.chains = 3,
+                        n.burn = 1000,
+                        n.iter = 5000,
+                        seed = 12345,
                         rho = 0.5) {
 
   # check class of object
