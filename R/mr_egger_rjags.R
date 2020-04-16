65da2c40a5829b031f765f628cd607c2f744380f
Tom Palmer <remlapmot@hotmail.com>
2020-04-16 10:41:20 +0100

    helpfile link

--- R/mr_egger_rjags.R
+++ R/mr_egger_rjags.R
@@ -2,7 +2,7 @@
 #'
 #' Bayesian implementation of the MR-Egger model with choice of prior distributions fitted using JAGS.
 #'
-#' @param object A data object of class `mr_format`
+#' @param object A data object of class [`mr_format`].
 #' @param prior A character string for selecting the prior distributions;
 #'
 #' * `"default"` selects a non-informative set of priors;
