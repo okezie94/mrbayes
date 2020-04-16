3f38a9ad3baf588d03951927f0cc20124576d939
Tom Palmer <remlapmot@hotmail.com>
2020-04-16 10:53:17 +0100

    formatting and defaults

--- R/mr_ivw_stan.R
+++ R/mr_ivw_stan.R
@@ -2,12 +2,16 @@
 #'
 #' Bayesian inverse variance weighted model with a choice of prior distributions fitted using RSTAN.
 #'
-#' @param data A data of class mr_format
-#' @param prior An integer for selecting the prior distributions; "1" selects a non-informative set of priors; "2" selects weakly informative priors; "3" selects a pseudo-horseshoe prior on the causal effect.
-#' @param n.chains Numeric indicating the number of chains used in the HMC estimation in rstan, the default is 1 chain.
-#' @param n.burn Numeric indicating the burn-in period of the Bayesian HMC estimation. The default is 200 samples.
-#' @param n.iter Numeric indicating the number of iterations in the Bayesian MCMC estimation. The default is 1000 iterations.
-#' @param seed Numeric indicating the random number seed. The default is 234.
+#' @param data A data of class [`mr_format`].
+#' @param prior An integer for selecting the prior distributions;
+#'
+#' * `1` selects a non-informative set of priors;
+#' * `2` selects weakly informative priors;
+#' * `3` selects a pseudo-horseshoe prior on the causal effect.
+#' @param n.chains Numeric indicating the number of chains used in the HMC estimation in rstan, the default is `3` chains.
+#' @param n.burn Numeric indicating the burn-in period of the Bayesian HMC estimation. The default is `1000` samples.
+#' @param n.iter Numeric indicating the number of iterations in the Bayesian MCMC estimation. The default is `5000` iterations.
+#' @param seed Numeric indicating the random number seed. The default is `12345`.
 #'
 #' @references Burgess, S., Butterworth, A., Thompson S.G. Mendelian randomization analysis with multiple genetic variants using summarized data. Genetic Epidemiology, 2013, 37, 7, 658-665 <https://dx.doi.org/10.1002/gepi.21758>.
 #'
@@ -17,10 +21,10 @@
 #' @export
 mr_ivw_stan <- function(data,
                         prior = 1,
-                        n.chains = 1,
-                        n.burn = 200,
-                        n.iter = 1000,
-                        seed = 234) {
+                        n.chains = 3,
+                        n.burn = 1000,
+                        n.iter = 5000,
+                        seed = 12345) {
 
   # check class of object
   if (!("mr_format" %in% class(data))) {
