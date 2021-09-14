d07f2650695e5a244873b37daed9189097789a89
Tom Palmer <remlapmot@hotmail.com>
2021-09-14 22:20:38 +0100

    put dontrun brackets on new lines

--- R/mr_ivw_rjags.R
+++ R/mr_ivw_rjags.R
@@ -28,14 +28,16 @@
 #' @references Burgess, S., Butterworth, A., Thompson S.G. Mendelian randomization analysis with multiple genetic variants using summarized data. Genetic Epidemiology, 2013, 37, 7, 658-665 \doi{10.1002/gepi.21758}.
 #'
 #' @examples
-#' \dontrun{fit <- mr_ivw_rjags(bmi_insulin)
+#' \dontrun{
+#' fit <- mr_ivw_rjags(bmi_insulin)
 #' print(fit)
 #' summary(fit)
 #' plot(fit$samples)
 #' # 90% credible interval
 #' fitdf <- do.call(rbind.data.frame, fit$samples)
 #' cri90 <- quantile(fitdf$Estimate, probs = c(0.05,0.95))
-#' print(cri90)}
+#' print(cri90)
+#' }
 #'
 mr_ivw_rjags <- function(object,
                          prior = "default",
