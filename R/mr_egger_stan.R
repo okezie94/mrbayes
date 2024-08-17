bdf08aae473c1af42dcc2f6e65e31f47380db1d4
Tom Palmer <remlapmot@hotmail.com>
2024-08-17 21:17:03 +0100

    Use examplesIf

--- R/mr_egger_stan.R
+++ R/mr_egger_stan.R
@@ -23,14 +23,10 @@
 #'
 #' @export
 #'
-#' @examples
-#' \donttest{
-#' if (require("rstan", quietly = TRUE)) {
-#'   # Note we recommend setting n.burn and n.iter to larger values
-#'   egger_fit <- mr_egger_stan(bmi_insulin, n.burn = 500, n.iter = 1000)
-#'   print(egger_fit)
-#' }
-#' }
+#' @examplesIf requireNamespace("rstan", quietly = TRUE)
+#' # Note we recommend setting n.burn and n.iter to larger values
+#' egger_fit <- mr_egger_stan(bmi_insulin, n.burn = 500, n.iter = 1000)
+#' print(egger_fit)
 mr_egger_stan <- function(data,
                         prior = 1,
                         n.chains = 3,
