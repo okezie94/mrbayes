d5d449715200ba6b9427038864928a67a3bf0eec
Tom Palmer <remlapmot@hotmail.com>
2024-08-17 21:28:04 +0100

    Add donttest

--- R/mr_egger_stan.R
+++ R/mr_egger_stan.R
@@ -24,9 +24,11 @@
 #' @export
 #'
 #' @examplesIf requireNamespace("rstan", quietly = TRUE)
+#' \donttest{
 #' # Note we recommend setting n.burn and n.iter to larger values
 #' egger_fit <- mr_egger_stan(bmi_insulin, n.burn = 500, n.iter = 1000)
 #' print(egger_fit)
+#' }
 mr_egger_stan <- function(data,
                         prior = 1,
                         n.chains = 3,
